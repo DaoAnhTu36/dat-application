94196bcb00e7dc5bfb4e97b4614c2f76d5ef0387
DAOANHTU <trailangnd96@gmail.com>
2025-01-08 02:10:49 +0700

    [ADD] CMS and Portal

---
+++ shop-food-portal/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { VendingMachineComponent } from './modules/vending-machine/vending-machine.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: VendingMachineComponent,
+  },
+];
